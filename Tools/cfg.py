80290a5d62457bf92747f3e3ce2e3557a1bb2fd7
Nelon <1245272985@qq.com>
2023-02-24 11:53:14 +0800

    创建项目框架

---
+++ Tools/cfg.py
@@ -0,0 +1,16 @@
+# author: Feng
+# contact: 1245272985@qq.com
+# datetime:2023/2/24 11:44
+# software: PyCharm
+"""
+文件说明：配置文件
+
+"""
+# 数据集存放位置
+TrainData = '../Data/'
+TestData = '../Data'
+
+# 参数设置
+EPOCH = 100
+LEARNING_RATE = 1e-5
+BATCH_SIZE = 2
